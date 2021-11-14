5f3b62f46a5914a997b763acf6bb337130a35871
PCY <pcy4196@naver.com>
2021-11-14 22:22:49 +0900

    21.11.14 commit -- Web 계층 개발    4. 상품 등록, 5. 상품 목록

---
+++ src/main/java/jpaBook/springJpa01/controller/ItemController.java
@@ -0,0 +1,49 @@
+package jpaBook.springJpa01.controller;
+
+import jpaBook.springJpa01.domain.item.Book;
+import jpaBook.springJpa01.domain.item.Item;
+import jpaBook.springJpa01.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        // 생성자로 데이터 값을 넣는것이 더 좋은 소스 --> setter 삭제로 구현 추천
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    /**
+     * 상품 목록
+     */
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
